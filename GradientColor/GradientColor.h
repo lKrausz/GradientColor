9d12b41b9e798f486f1c99583ab48f36ffe6ac58
Vi <krausz.st@gmail.com>
2021-08-03 22:40:12 +0300

    gradient framework

---
+++ GradientColor/GradientColor.h
@@ -0,0 +1,18 @@
+//
+//  GradientColor.h
+//  GradientColor
+//
+//  Created by Виктория Козырева on 03.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GradientColor.
+FOUNDATION_EXPORT double GradientColorVersionNumber;
+
+//! Project version string for GradientColor.
+FOUNDATION_EXPORT const unsigned char GradientColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientColor/PublicHeader.h>
+
+
---
+++ GradientColor/GradientColor.swift
@@ -0,0 +1,20 @@
+//
+//  GradientColor.swift
+//  GradientColor
+//
+//  Created by Виктория Козырева on 03.08.2021.
+//
+
+import UIKit
+
+class GradientColorView: UIView {
+    
+    func gradient(first: UIColor, second: UIColor, for view: UIView) {
+        let gradient = CAGradientLayer()
+        gradient.colors = [first.cgColor, second.cgColor]
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        view.layer.addSublayer(gradient)
+    }
+}
+
